39e766402b7bd756fe9b15836643a5f4a467a423
Miguel Thomas Ruberta <miguelruberta@gmail.com>
2020-04-17 12:10:51 +0700

    book form

---
+++ app/src/main/java/com/example/viaggio/BookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viaggio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book)
+    }
+}
---
+++ app/src/main/java/com/example/viaggio/DoublePassActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viaggio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoublePassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_double_pass)
+    }
+}
